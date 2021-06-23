2750343c4acb3916ff440578190092082232716c
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-06-23 18:47:03 -0300

    Exercicio 2 - corrigido

--- fundamentos/bloco4-javascript/dia1-primeiros-passos/exercicio2.js
+++ fundamentos/bloco4-javascript/dia1-primeiros-passos/exercicio2.js
@@ -1,5 +1,14 @@
-let a = 130;
+let a = 185;
 let b = 123;
 
-result = a > b;
-console.log(result);
+if (a > b) {
+  console.log(a)
+}
+
+else if (a < b){
+  console.log(b)
+}
+
+else {
+  console.log("Os números são iguais")
+}
