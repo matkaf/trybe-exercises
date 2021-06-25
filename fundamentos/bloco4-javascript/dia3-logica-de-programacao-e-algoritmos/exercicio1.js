a12765f82b33e16b689666c02ac779c888c8a2e6
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-06-25 20:01:18 -0300

    Finalidos: Exercicio 1  e 2

--- fundamentos/bloco4-javascript/dia3-logica-de-programacao-e-algoritmos/exercicio1.js
+++ fundamentos/bloco4-javascript/dia3-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -1,13 +1,16 @@
 // 1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
-// let n = 5;
-// let square = [];
+// let n = 10;
+// let square = '';
 
 // for (i = 0; i < n; i += 1){
-//   square = '*****'
+//   square += '*';
+// }
+
+// for (i = 0; i < n; i+= 1){
 //   console.log(square)
 // }
 
-// // 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+// 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
 // let n = 5;
 // let square = [];
 
@@ -15,3 +18,5 @@
 //   square += '*'
 //   console.log(square)
 // }
+
+
