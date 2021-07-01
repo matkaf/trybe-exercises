2a56cbee07e918edaaac6440cc36be89cf42cc57
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-07-01 10:02:41 -0300

    Exercicio 1

---
+++ fundamentos/bloco5-DOM-Eventos-e-WebStorage/dia1-DOM-e-seletores/script.js
@@ -0,0 +1,9 @@
+document.querySelector('header').style.backgroundColor = '#2fc18c';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
+
+// for (let i = 0; i < document.querySelectorAll('.emergency-tasks h3').length; i += 1){
+//   return document.querySelectorAll('.emergency-tasks h3')[i].style.backgroundColor = 'purple';
+}
+
+
+
