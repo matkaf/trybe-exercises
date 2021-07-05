2d64dc29a6af2c09d0e11fada78d9e18682ac7b3
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-07-05 10:11:05 -0300

    Exerciio de Fixacao Finalizado

--- fundamentos/bloco5-DOM-Eventos-e-WebStorage/dia1-DOM-e-seletores/script.js
+++ fundamentos/bloco5-DOM-Eventos-e-WebStorage/dia1-DOM-e-seletores/script.js
@@ -1,9 +1,15 @@
 document.querySelector('header').style.backgroundColor = '#2fc18c';
-document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
 
-// for (let i = 0; i < document.querySelectorAll('.emergency-tasks h3').length; i += 1){
-//   return document.querySelectorAll('.emergency-tasks h3')[i].style.backgroundColor = 'purple';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'pink';
+let emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+for (let i = 0; i < emergencyTasksHeaders.length; i += 1) {
+  emergencyTasksHeaders[i].style.backgroundColor = 'magenta';
 }
 
+document.querySelector('.no-emergency-tasks').style.backgroundColor = '#f5ff66';
+let noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+for (let ii = 0; ii < noEmergencyTasksHeaders.length; ii += 1){
+  noEmergencyTasksHeaders[ii].style.backgroundColor = 'black';
+}
 
-
+document.querySelector('footer').style.backgroundColor = '#0d1f26';
