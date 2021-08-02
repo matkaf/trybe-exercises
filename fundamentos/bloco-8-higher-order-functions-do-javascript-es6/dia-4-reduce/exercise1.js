06307c0dca7017c55db3d488bbfd01c29170d121
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-08-02 19:23:34 -0300

    Exercicio 1

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-reduce/exercise1.js
@@ -0,0 +1,20 @@
+// 1 - Dada uma matriz, transforme em um array.
+
+const assert = require('assert');
+
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+function flatten() {
+
+// array.concat = retorna um novo array contendo todos os valores passados como parametro.
+  return arrays.reduce((acc, curr) => acc.concat(curr)) 
+
+}
+
+
+console.log(flatten());
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
