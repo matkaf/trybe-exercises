a8e7c8905770d4eef998cb589e5707e2f94dda52
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-08-02 23:56:40 -0300

    Exercicio 6

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-reduce/exercise5.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-reduce/exercise5.js
@@ -8,6 +8,8 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
+// Não consegui resolver: Eu tinha feito até o split numa const e depois feito o outro reduce, mas não deu certo. Tudo era igual...
+
 function containsA() {
   // escreva seu código aqui
  return names.reduce((acc, curr) => acc + curr.split('').reduce((acc, curr) => {
