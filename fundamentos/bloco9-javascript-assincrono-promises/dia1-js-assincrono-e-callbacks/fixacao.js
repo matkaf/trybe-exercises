bf28103fd5f900f0f9bbafe75af00eae6984b14a
Matheus Kaffka <matheus.kaffka@gmail.com>
2021-08-23 20:18:22 -0300

    Exercicio 4

--- fundamentos/bloco9-javascript-assincrono-promises/dia1-js-assincrono-e-callbacks/fixacao.js
+++ fundamentos/bloco9-javascript-assincrono-promises/dia1-js-assincrono-e-callbacks/fixacao.js
@@ -1,3 +1,41 @@
+// Fixacao de callbacks:
+
+// const myExpenses = [
+//   {
+//     gym: 99,
+//   },
+//   {
+//     ifood: 200,
+//   },
+//   {
+//     phone: 60,
+//   },
+//   {
+//     internet: 100,
+//   },
+// ];
+
+// const myIncome = 1000;
+
+// const monthlyBudget = (myIncome, myExpenses, callback) => {
+
+//   const totalExpenses = callback(myExpenses);
+//   const totalAfterExpenses = myIncome - totalExpenses;
+
+//   console.log(`Balanço do mês:
+//     Recebido: R$${myIncome},00
+//     Gasto: R$${totalExpenses},00
+//     Saldo: R$${totalAfterExpenses},00 `);
+// };
+
+// const handleExpenses = myExpenses => {
+//   const eachValue = myExpenses.map((item) => Object.values(item)[0]);
+//   const totalExpense = eachValue.reduce((acc, curr) => acc + curr , 0);
+//   return totalExpense;
+// };
+
+// monthlyBudget(myIncome, myExpenses, handleExpenses);
+
 // const assert = require('assert');
 
 // const pushNumber = (list, number) => {
@@ -15,19 +53,52 @@
 
 // Fixacao de Callbacks:
 
-const assert = require('assert');
+// const assert = require('assert');
 
-const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+// const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const getUser = (callback) => {
-  const userToReturn = {
-    firstName: "Ivan",
-    lastName: "Ivanovich",
-    nationality: "Russian"
-  };
-  return callback(userToReturn);
+// const getUser = (callback) => {
+//   const userToReturn = {
+//     firstName: "Ivan",
+//     lastName: "Ivanovich",
+//     nationality: "Russian"
+//   };
+//   return callback(userToReturn);
+// };
+
+// assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
+// assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
+
+// Fixação de Assincronicidade:
+
+const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onFailure) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    }
+    else {
+      const errorMessage = "Country could not be found";
+      onFailure(errorMessage); // código adicionado
+    }
+  }, delay());
 };
 
-assert.strictEqual(getUser(userFullName), "Hello! My name is Ivan Ivanovich"); // complete a chamada da função de getUser
-assert.strictEqual(getUser(userNationality), "Ivan is Russian"); // complete a chamada da função de getUser
+// Deve imprimir "Returned country is Brazil" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em falha
+getCountry(countryCurrency, printErrorMessage);
